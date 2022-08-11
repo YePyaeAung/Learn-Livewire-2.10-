28b12c24e6ad25c7d946185b271bcddf848cf8bd
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-11 22:08:22 +0630

    Livewire Property Binding in Form Component

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Form;
 use App\Http\Livewire\Home;
 use App\Http\Livewire\Post;
 use App\Http\Livewire\User;
@@ -23,3 +24,4 @@ Route::get('/', function () {
 Route::get('/post', Post::class);
 Route::get('/user', User::class);
 Route::get('/home/{name?}', Home::class);
+Route::get('/form', Form::class);
