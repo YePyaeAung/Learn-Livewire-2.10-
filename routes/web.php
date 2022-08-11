fbf7c0b2196c43020b4125969993fb3571498636
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-11 21:20:59 +0630

    Create Post Component and User (inline) Component

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Post;
+use App\Http\Livewire\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', Post::class);
+Route::get('/user', User::class);
