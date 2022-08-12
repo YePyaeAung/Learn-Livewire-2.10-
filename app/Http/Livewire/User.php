968164400583fef2206a572826693ffa19864b76
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-12 22:55:40 +0630

    Livewire With Database

--- app/Http/Livewire/User.php
+++ app/Http/Livewire/User.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User as ModelsUser;
 use Livewire\Component;
 
 class User extends Component
 {
+    public $users;
+
     public function render()
     {
-        return <<<'blade'
-            <div>
-                <h1>This is User inline Component.</h1>
-            </div>
-        blade;
+        $this->users = ModelsUser::all();
+        return view('livewire.user');
     }
 }
